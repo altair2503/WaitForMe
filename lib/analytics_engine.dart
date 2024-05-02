03d849682a8ce65a8b1b82ba1da9a3d88dba78d0
Altair <altair200294@gmail.com>
2024-05-02 15:36:48 +0500

    Analytics engine added

---
+++ lib/analytics_engine.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:wait_for_me/models/stops_model.dart';
+
+class AnalyticsEngine {
+  static final _instance = FirebaseAnalytics.instance;
+
+
+  static void selectedStation(Stop stop, DateTime time) async{
+    return _instance.logEvent(name: "Selected bus stop",  parameters: {"Bus stop name": stop, "Time": time});
+  }
+
+  static void getOffStation(Stop stop, DateTime time) async{
+    return _instance.logEvent(name: "Get off station",  parameters: {"Station name": stop, "Time": time});
+  }
+
+  static void onBusActivity(Position currentPosition, Stop stop, DateTime time) async{
+    return _instance.logEvent(name: "On bus activity",  parameters: {"Current position": "${currentPosition.latitude}, ${currentPosition.longitude}", "Selected station": stop, "Time": time});
+  }  
+
+  static void cancelActivity(Position currentPosition, Stop stop, DateTime time) async{
+    return _instance.logEvent(name: "Cancel pressed",  parameters: {"Current position": "${currentPosition.latitude}, ${currentPosition.longitude}", "Selected station": stop, "Time": time});
+  } 
+}
